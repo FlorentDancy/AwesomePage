2fea8928c545a8a14c8af41de7e2eb8c1c2ace48
FlorentDancy <florent.dancy.1@ulaval.ca>
2014-07-23 20:08:16 +0200

    HTML - 3.4 RAF : Fixer bidouillage pour icon sur grand écran

--- js/app.js
+++ js/app.js
@@ -4,13 +4,27 @@
 
 $(document).ready(function(){
 
+    var compteur=1;
+    $('#left-menu').on( "click", function() {
+        if (window.matchMedia("(min-width: 40.063em)").matches) {
+            $('#left-menu').sidr({displace:false});
+               if (compteur==0){
+                   $( this ).css( "margin-left", "260px" );
+                   compteur=1;
+               } else{
+                   $( this ).css( "margin-left", "0px" );
+                   compteur=0;
+               }
+        } else {
+            $('#left-menu').sidr();
+            if (compteur==0){
 
-    if (window.matchMedia("(min-width: 40.063em)").matches) {
-        $('#left-menu').sidr({displace:false});
-    } else {
-        $('#left-menu').sidr();
-    }
-
+                compteur=1;
+            } else{
+                compteur=0;
+            }
+        }
+    });
     $(document).foundation();
 
 });
